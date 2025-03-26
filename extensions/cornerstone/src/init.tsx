a3f5e5616c1e45a945e9045b8149b16b4a4359b7
thibaut-etienne-deemea <thibaut.etienne@deemea.com>
2025-03-26 10:54:13 +0100

    Fix cors, try 1

--- extensions/cornerstone/src/init.tsx
+++ extensions/cornerstone/src/init.tsx
@@ -107,11 +107,14 @@ export default async function init({
   window.extensionManager = extensionManager;
   window.commandsManager = commandsManager;
 
+  console.log('CROSS origin 1', appConfig.showWarningMessageForCrossOrigin);
+  console.log('CROSS origin 2', window.crossOriginIsolated);
+  console.log('CROSS origin 3', sharedArrayBufferDisabled);
+
   if (
     appConfig.showWarningMessageForCrossOrigin &&
     !window.crossOriginIsolated &&
-    !sharedArrayBufferDisabled &&
-    false
+    !sharedArrayBufferDisabled
   ) {
     uiNotificationService.show({
       title: 'Cross Origin Isolation',
