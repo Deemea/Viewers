d408a464203b1623156ede37b6fceb3b8008f756
thibaut-etienne-deemea <thibaut.etienne@deemea.com>
2025-04-25 17:54:01 +0200

    Add points color

---
+++ deemea-extension/src/utils/pointsColors.ts
@@ -0,0 +1,105 @@
+import { Palette } from './palette';
+
+export const points = [
+  {
+    name: 'acromion_lateral',
+    color: Palette.Blue,
+    highlighted: Palette.DarkBlue,
+  },
+  {
+    name: 'trochiter_lateral',
+    color: Palette.Turquoise,
+    highlighted: Palette.DarkTurquoise,
+  },
+  {
+    name: 'glenoid_superior',
+    color: Palette.Purple,
+    highlighted: Palette.DarkPurple,
+  },
+  {
+    name: 'glenoid_inferior',
+    color: Palette.Purple,
+    highlighted: Palette.DarkPurple,
+  },
+  {
+    name: 'coracoid_external',
+    color: Palette.Purple,
+    highlighted: Palette.DarkPurple,
+  },
+  {
+    name: 'humerus_proximal',
+    color: Palette.Yellow,
+    highlighted: Palette.DarkYellow,
+  },
+  {
+    name: 'humerus_distal',
+    color: Palette.Yellow,
+    highlighted: Palette.DarkYellow,
+  },
+  {
+    name: 'supraspinal_fossa_medial',
+    color: Palette.Red,
+    highlighted: Palette.DarkRed,
+  },
+  {
+    name: 'supraspinal_fossa_lateral',
+    color: Palette.Red,
+    highlighted: Palette.DarkRed,
+  },
+  {
+    name: 'scapula_border_medial',
+    color: Palette.Pink,
+    highlighted: Palette.DarkPink,
+  },
+  {
+    name: 'scapula_border_lateral',
+    color: Palette.Pink,
+    highlighted: Palette.DarkPink,
+  },
+  {
+    name: 'glenosphere_inferior',
+    color: Palette.LightBlue,
+    highlighted: Palette.DarkLightBlue,
+  },
+
+  {
+    name: 'glenosphere_superior',
+    color: Palette.LightBlue,
+    highlighted: Palette.DarkLightBlue,
+  },
+
+  {
+    name: 'glenoid_pivot_point',
+    color: Palette.Pink,
+    highlighted: Palette.DarkPink,
+  },
+
+  {
+    name: 'humeral_plateau_inferior',
+    color: Palette.Orange,
+    highlighted: Palette.DarkOrange,
+  },
+
+  {
+    name: 'humeral_plateau_superior',
+    color: Palette.Orange,
+    highlighted: Palette.DarkOrange,
+  },
+
+  {
+    name: 'trochiter_medial',
+    color: Palette.Turquoise,
+    highlighted: Palette.DarkTurquoise,
+  },
+  {
+    name: 'tibia_plateau_exterior',
+    color: Palette.Red,
+    highlighted: Palette.DarkRed,
+  },
+  // tibia_plateau_interior
+  {
+    name: 'tibia_plateau_interior',
+    color: Palette.Red,
+    highlighted: Palette.DarkRed,
+  },
+];
