c471084dd29105821ee6706909a5e5ccad41370b
thibaut-etienne-deemea <thibaut.etienne@deemea.com>
2025-08-21 17:19:33 +0200

    save/display/update seg

--- extensions/cornerstone/src/utils/segmentationHandlers.ts
+++ extensions/cornerstone/src/utils/segmentationHandlers.ts
@@ -9,6 +9,7 @@ import axios from 'axios';
 export function setupSegmentationDataModifiedHandler({
   segmentationService,
   customizationService,
+  displaySetService,
   commandsManager,
   extensionManager,
 }) {
@@ -61,8 +62,9 @@ export function setupSegmentationDataModifiedHandler({
 
       // SAVE AUTO SEGMENTATION
       try {
+        const displaySets = displaySetService.getActiveDisplaySets();
+        const segDisplaySets = displaySets.filter(ds => ds.Modality === 'SEG');
         const segmentations = segmentationService.getSegmentations();
-        console.log('segmentationssegmentations', segmentations);
 
         const defaultDataSource = extensionManager.getActiveDataSource();
         const generatedData = await commandsManager.run('generateSegmentation', {
@@ -72,28 +74,27 @@ export function setupSegmentationDataModifiedHandler({
           },
         });
 
-        console.log('generatedData', generatedData);
-
         if (!generatedData || !generatedData.dataset) {
           throw new Error('Error during segmentation generation');
         }
 
-        console.log('GENERATED DATA AUTO', generatedData);
+        // Suppression avant ajout pour modification
+        console.log('segDisplaySets', segDisplaySets);
 
-        // const sop = '2.25.469580321458735619682144517728794524840';
-        // const series = '2.25.661569102649699786369103167050009005028';
-        // const deleteUrl = `http://localhost:8080/dcm4chee-arc/aets/DCM4CHEE/rs/studies/1.2.840.113704.1.111.4556.1312266885.1/series/${series}/reject/113039%5EDCM`;
-
-        // const response = await axios.post(deleteUrl);
-
-        // console.log('responseresponse', response);
+        if (segDisplaySets.length > 0) {
+          const sop = segDisplaySets[0].SOPInstanceUID;
+          const series = segDisplaySets[0].SeriesInstanceUID;
+          const study = segDisplaySets[0].StudyInstanceUID;
+          console.log('SOP TO DELETE', sop);
+          const deleteUrl = `http://localhost:8080/dcm4chee-arc/aets/DCM4CHEE/rs/studies/${study}/series/${series}/reject/113039%5EDCM`;
+          await axios.post(deleteUrl);
+          displaySetService.deleteDisplaySet(segDisplaySets[0].displaySetInstanceUID);
+        }
 
         const { dataset: naturalizedReport } = generatedData;
-        naturalizedReport.SeriesDescription = 'TEST SAVE AUTO new';
-        // naturalizedReport.SOPClassUID = '1.2.840.10008.5.1.4.1.1.66.4';
-        // naturalizedReport.SOPInstanceUID = '2.25.724506402023991505305749101187102926116';
-        // naturalizedReport.SeriesInstanceUID = '2.25.661569102649699786369103167050009005028';
-        console.log('CREATED auto', naturalizedReport);
+        console.log('naturalizedReport', naturalizedReport);
+
+        naturalizedReport.SeriesDescription = 'TEST SAVE AUTO new3';
 
         await defaultDataSource[0].store.dicom(naturalizedReport);
 
@@ -106,8 +107,6 @@ export function setupSegmentationDataModifiedHandler({
           {
             type: OHIFMessageType.SAVE_SEGMENTATION,
             message: {
-              studyInstanceUID: naturalizedReport.StudyInstanceUID,
-              SOPInstanceUID: naturalizedReport.SOPInstanceUID,
               seriesInstanceUID: naturalizedReport.SeriesInstanceUID,
             },
           },
