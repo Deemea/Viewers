cab3050da301ae3cee9014ac41a6766d25bfcbf7
thibaut-etienne-deemea <thibaut.etienne@deemea.com>
2025-09-05 17:18:26 +0200

    Clean project

--- deemea-extension-3d/src/index.tsx
+++ deemea-extension-3d/src/index.tsx
@@ -1,56 +1,24 @@
-import { id } from './id';
-import React from 'react';
+// import commandsModule from './commandsModule';
+// const onModeInit = false;
 
-import getSopClassHandlerModule from './getSopClassHandlerModule';
-import getHangingProtocolModule from './getHangingProtocolModule';
-import getCommandsModule from './commandsModule';
-import { getToolbarModule } from './getToolbarModule';
-
-const Component = React.lazy(() => {
-  return import(/* webpackPrefetch: true */ './viewports/OHIFCornerstoneSEGViewport');
-});
-
-const OHIFCornerstoneSEGViewport = props => {
-  return (
-    <React.Suspense fallback={<div>Loading...</div>}>
-      <Component {...props} />
-    </React.Suspense>
-  );
-};
-
-/**
- * You can remove any of the following modules if you don't need them.
- */
 const extension = {
-  /**
-   * Only required property. Should be a unique value across all extensions.
-   * You ID can be anything you want, but it should be unique.
-   */
-  id,
-  getCommandsModule,
-  getToolbarModule,
-  getViewportModule({ servicesManager, extensionManager, commandsManager }) {
-    const ExtendedOHIFCornerstoneSEGViewport = props => {
-      return (
-        <OHIFCornerstoneSEGViewport
-          servicesManager={servicesManager}
-          extensionManager={extensionManager}
-          commandsManager={commandsManager}
-          {...props}
-        />
-      );
-    };
+  id: 'deemea-extension-3d',
 
-    return [{ name: 'dicom-seg', component: ExtendedOHIFCornerstoneSEGViewport }];
+  preRegistration({ servicesManager, commandsManager, configuration = {} }) {
+    // Any pre-registration tasks
   },
-  /**
-   * SopClassHandlerModule should provide a list of sop class handlers that will be
-   * available in OHIF for Modes to consume and use to create displaySets from Series.
-   * Each sop class handler is defined by a { name, sopClassUids, getDisplaySetsFromSeries}.
-   * Examples include the default sop class handler provided by the default extension
-   */
-  getSopClassHandlerModule,
-  getHangingProtocolModule,
+
+  onModeEnter: ({ extensionManager, servicesManager, commandsManager }) => {
+    const { measurementService } = servicesManager.services;
+
+    measurementService.clearMeasurements();
+  },
+
+  onModeExit: ({ servicesManager, commandsManager }) => {
+    const { measurementService } = servicesManager.services;
+    console.log('onModeExit', commandsManager.getContext());
+    measurementService.reset();
+  },
 };
 
 export default extension;
