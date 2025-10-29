787dcfc451f28ea734cc01d84c41fce33de18158
thibaut-etienne-deemea <thibaut.etienne@deemea.com>
2025-10-29 16:57:01 +0100

    [#3246] (feat): Integrate anteversion acetabulaire on hippolyne (#107)

--- deemea-extension/src/utils/relatedPoint.ts
+++ deemea-extension/src/utils/relatedPoint.ts
@@ -1,33 +1,33 @@
 export interface Point {
-    id: number;
-    x: number;
-    y: number;
-    xOrigin: number;
-    yOrigin: number;
-    name: string;
-    lastModificationDate: Date;
-    lastModificationUserId?: string;
+  id: number;
+  x: number;
+  y: number;
+  xOrigin: number;
+  yOrigin: number;
+  name: string;
+  lastModificationDate: Date;
+  lastModificationUserId?: string;
 }
 
 export interface ImagingData {
-    id: number;
-    isActive: boolean;
-    measureName: string;
-    isFiltrable: boolean;
-    custom: boolean;
-    numberOfPoints: number;
-    laterality: string;
-    unit: string;
-    bodyPart: string;
-    view: string;
-    color: string;
+  id: number;
+  isActive: boolean;
+  measureName: string;
+  isFiltrable: boolean;
+  custom: boolean;
+  numberOfPoints: number;
+  laterality: string;
+  unit: string;
+  bodyPart: string;
+  view: string;
+  color: string;
 }
 
 export interface RelatedPoint {
-    points: Point[];
-    imagingData: ImagingData;
-    forceHide: boolean
-    hide: boolean | null;
-    locked: boolean;
-    measurementId: number;
+  points: Point[];
+  imagingData: ImagingData;
+  forceHide: boolean;
+  hide: boolean | null;
+  locked: boolean;
+  measurementId: number;
 }
